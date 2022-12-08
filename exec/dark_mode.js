2fc76037cc105b19a42be4360022cb0d7cf31709
Hugo Goorickx <hugo@goorickx.be>
2022-12-08 09:07:20 +0100

    Corrections mineures

--- exec/dark_mode.js
+++ exec/dark_mode.js
@@ -1,3 +1,97 @@
+/** Change le theme en fonction du status de e
+ *  Details:
+ *  1       Definie le theme comme light par defaut
+ *  2       Si e est checked alors le theme passe en noir
+ *  3       Modifie le theme sur le document, en stockage local et sur la variable actualTheme (utilise pour changeColor)
+ *  4       Execute changeColor qui va s'occuper de changer globalement le theme
+ * 
+ * Params:
+ *      e   =>   Ensemble des infos fournies par le addEventListener
+ * 
+ * Retour:
+ *      Rien
+ */
+function switchTheme(e)
+{
+    let theme = 'light';//1
+    if (e.target.checked)//2
+        theme = 'dark';
+    document.documentElement.setAttribute('data-theme', theme);//3
+    localStorage.setItem('theme', theme);
+    actualTheme = theme;
+    changeColor(); //4
+}
+
+/** Genere le code html pour les fleches des deux carrouselles
+ *  Details:
+ *      1       Cree la balise img
+ *      2       Ajoute la source
+ *      3       Revoit la balise
+ * 
+ *  Params:
+ *      src     =>      Chemin d'acces vers l'img de la fleche
+ * 
+ *  Return:
+ *      fleche  =>      Balise img creee dans la fonction
+ */
+function gen_fleche(src)
+{
+    let fleche = document.createElement('img');
+    fleche.setAttribute('src', src);
+    return (fleche);
+}
+
+/** Genere les 4 fleches avec une couleur en fonction du theme
+ *  Details:
+ *      1       Suppression des fleches pour le premier carrosuelle
+ *      2       Suppression des fleches pour le deuxieme carrosuelle
+ *      3       Ajoute les 4 fleches
+ * 
+ *  Params:
+ *      fl_gc   =>      Fleche de gauche
+ *      fl_dr   =>      Fleche de droite
+ * 
+ *  Return:
+ *      Rien
+ */
+function light(fl_gc, fl_dr) 
+{
+    for (let elem of document.getElementsByClassName('fl_gr'))//1
+        if (elem.getElementsByTagName('img').length !=0)
+            elem.removeChild(elem.getElementsByTagName('img')[0]);
+
+    for (let elem of document.getElementsByClassName('fl_pt'))//2
+        if (elem.getElementsByTagName('img').length !=0)
+            elem.removeChild(elem.getElementsByTagName('img')[0]);
+    
+    document.getElementsByClassName('fl_gr')[0].appendChild(fl_gc);//3
+    document.getElementsByClassName('fl_gr')[1].appendChild(fl_dr);
+    document.getElementsByClassName('fl_pt')[0].appendChild(fl_gc.cloneNode());
+    document.getElementsByClassName('fl_pt')[1].appendChild(fl_dr.cloneNode());
+}
+
+/** Change les couleurs des fleches en fonction du theme
+ *  Details:
+ *      1       Genere les fleches blanches si le theme est dark
+ *      2       Genere les fleches noirs si le theme est light
+ *  Params:
+ *      Rien
+ * 
+ *  Return:
+ *      Rien
+ */
+function changeColor()
+{
+    if (actualTheme === 'dark')
+        light(gen_fleche('imgs/white_arrow_left.png'), gen_fleche('imgs/white_arrow_right.png'));
+    else if (actualTheme === 'light')
+        light(gen_fleche('imgs/black_arrow_left.png'), gen_fleche('imgs/black_arrow_right.png'));
+}
+
+/////////////////////////////////////////////
+//      Exec                               //
+/////////////////////////////////////////////
+
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
 let actualTheme = currentTheme;
@@ -5,76 +99,10 @@ let actualTheme = currentTheme;
 if (currentTheme)
 {
     document.documentElement.setAttribute('data-theme', currentTheme);
-    if (currentTheme === 'dark') {
+    if (currentTheme === 'dark')
         toggleSwitch.checked = true;
-    }
-}
-
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark');
-        actualTheme = 'dark';
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light');
-        actualTheme = 'light';
-    } 
-    changeColor();  
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
 
-//en dessous
-
-let fleche_droite_noir = document.createElement('img');
-fleche_droite_noir.setAttribute('src', 'imgs/black_arrow_right.png');
-let fleche_droite_noir_copy = fleche_droite_noir.cloneNode();
-
-let fleche_gauche_noir = document.createElement('img');
-fleche_gauche_noir.setAttribute('src', 'imgs/black_arrow_left.png');
-let fleche_gauche_noir_copy = fleche_gauche_noir.cloneNode();
-
-let fleche_droite_blanche = document.createElement('img');
-fleche_droite_blanche.setAttribute('src', 'imgs/white_arrow_right.png');
-let fleche_droite_blanche_copy = fleche_droite_blanche.cloneNode();
-
-let fleche_gauche_blanche = document.createElement('img');
-fleche_gauche_blanche.setAttribute('src', 'imgs/white_arrow_left.png');
-let fleche_gauche_blanche_copy = fleche_gauche_blanche.cloneNode();
-
 changeColor();
-
-function light(fl_gc, fl_gc_copy, fl_dr, fl_dr_copy) {
-    for (let elem of document.getElementsByClassName('fl_gr'))
-    {
-        if (elem.getElementsByTagName('img').length !=0)
-        {
-            elem.removeChild(elem.getElementsByTagName('img')[0]);
-        }
-    }
-    for (let elem of document.getElementsByClassName('fl_pt'))
-    {
-        if (elem.getElementsByTagName('img').length !=0)
-        {
-            elem.removeChild(elem.getElementsByTagName('img')[0]);
-        }
-    }
-    document.getElementsByClassName('fl_gr')[0].appendChild(fl_gc);
-    document.getElementsByClassName('fl_gr')[1].appendChild(fl_dr);
-    document.getElementsByClassName('fl_pt')[0].appendChild(fl_gc_copy);
-    document.getElementsByClassName('fl_pt')[1].appendChild(fl_dr_copy);
-}
-
-function changeColor()
-{
-    console.log(actualTheme);
-    if (actualTheme === 'dark') {
-        light(fleche_gauche_blanche, fleche_gauche_blanche_copy, fleche_droite_blanche, fleche_droite_blanche_copy);
-    }
-    else if (actualTheme === 'light') {
-        light(fleche_gauche_noir, fleche_gauche_noir_copy, fleche_droite_noir, fleche_droite_noir_copy);
-    }
-
-}
--- exec/exec.js
+++ exec/exec.js
@@ -1,3 +1,119 @@
+/** Change l'image en focntion du sens dans le quel on va (1 = doite | -1 = gauche)
+ *  Details:
+ *      1       Se place la ou va ajouter l'iamge
+ *      2       Supprime l'ancienne image
+ *      3       Ajoute le deplacement
+ *      4       Si l'index est plus petit que 0 alors on remet a la fin du tableau
+ *              [0, 1, 2, 3]
+ *               0   =>   3
+ *      5       Si l'index est plus grand que la taille du tableau alors on remet au debut du tableau
+ *              [0, 1, 2, 3]
+ *               0   <=   3
+ *      6       On genere la nouvelle image   
+ *
+ *  Params:
+ *      move    =>  Valeur de declage (1 droite | -1 gauche)
+ * 
+ *  Return:
+ *      Rien
+ */
+function move_car_up(move)
+{
+    let pos_old_pic = document.getElementsByClassName('item_carrousel')[0]; //1
+    pos_old_pic.remove(); //2
+    index_carrousel = index_carrousel + move; //3
+    if (index_carrousel < 0) //4
+        index_carrousel = all_prod.length + move;
+    else if (index_carrousel == all_prod.length) //5
+        index_carrousel = 0;
+    all_prod[index_carrousel].gen_picture(); //6
+}
+
+/** Change l'image en focntion du sens dans le quel on va (1 = doite | -1 = gauche)
+ *  Details:
+ *      1       Se place la ou va ajouter l'iamge
+ *      2       Supprime l'ancienne image
+ *      3       Ajoute le deplacement
+ *      4       Si l'index est plus petit que 0 alors on remet a la fin du tableau
+ *              [0, 1, 2, 3]
+ *               0   =>   3
+ *      5       Si l'index est plus grand que la taille du tableau alors on remet au debut du tableau
+ *              [0, 1, 2, 3]
+ *               0   <=   3
+ *      6       On genere la nouvelle image
+ *      7       Ajoute l'event show_cat pour afficher les produits de sa categorie
+ *
+ *  Params:
+ *      move    =>  Valeur de declage (1 droite | -1 gauche)
+ * 
+ *  Return:
+ *      Rien
+ */
+function move_car_down(move)
+{
+    let pos_old_button = document.getElementsByClassName('menu')[0]; //1
+    pos_old_button.remove(); //2
+    index_button = index_button + move; //3
+    if(index_button < 0) //4
+        index_button = all_categories.length + move;
+    else if (index_button >= all_categories.length) //5
+        index_button = 0;
+    all_categories[index_button].gen_button(); //6
+    let pos_button = document.getElementsByClassName('menu')[0];//7
+    pos_button.addEventListener("click", show_cat);
+}
+
+/** Ajoute les articles de la bonne categories
+ *  Details:
+ *      1       Prends la position ou nous allons ajouter les produits
+ *      2       Extrait son nom
+ *      3       Supprime les anciens produits
+ *      4       Ajoute les nouvelles cartes
+ * 
+ *  Params:
+ *      Rien
+ * 
+ *  Return:
+ *      Rien
+ */
+function show_cat()
+{
+    let pos_old_article = document.getElementsByClassName('article'); //1
+    let name_cat = all_categories[index_button].name; //2
+
+    while (pos_old_article.length != 0) //3
+        pos_old_article[0].remove();
+
+    for (let elem of all_prod) //4
+        for (let single_cat of elem.category)
+            if (single_cat == name_cat)
+                elem.gen_card();
+}
+
+/** Renvoi les produits achetes et la somme totale
+ *  Details:
+ *      1       Genere la somme
+ *      2       Genere l'output
+ *      3       Ouvre une box alert avec l'output genere
+ * 
+ *  Params:
+ *      Rien
+ * 
+ *  Return:
+ *      Rien
+ */
+function show_order()
+{
+    let somme = 0;//1
+    for (let elem of test)
+        somme += parseInt(elem.price);
+    let output = "Vous avez commandez:\n";//2
+    for (let elem of test)
+        output += "    -" + elem.name + " : " + elem.price + "$\n";
+    output += "Pour un total de " + somme +"$";
+    window.alert(output);//3
+}
+
 //Import le fichier json
 import file from '../menu.json' assert {type: 'json'};
 import {Button} from './Button.js';
@@ -27,111 +143,27 @@ for (let elem of file.menu)
     }
 }
 
-function car_left()
-{
-    let pos_old_pic = document.getElementsByClassName('item_carrousel')[0];
-    pos_old_pic.remove();
-    index_carrousel = index_carrousel - 1;
-    if (index_carrousel < 0)
-        index_carrousel = all_prod.length - 1;
-    all_prod[index_carrousel].gen_picture();    
-}
-
-function car_right()
-{
-    let pos_old_pic = document.getElementsByClassName('item_carrousel')[0];
-    pos_old_pic.remove()
-    index_carrousel = index_carrousel + 1;
-    if (index_carrousel >= all_prod.length)
-        index_carrousel = 0;
-    all_prod[index_carrousel].gen_picture();
-}
-
-//  0        <=          7 8
-// [1, 2, 3, 4, 5, 6, 7, 8]... => 8
-
-function button_left()
-{
-    let pos_old_button = document.getElementsByClassName('menu')[0];
-    pos_old_button.remove();
-    index_button = index_button - 1;
-    if(index_button < 0)
-        index_button = all_categories.length -1;
-    all_categories[index_button].gen_button();
-    let pos_button = document.getElementsByClassName('menu')[0];
-    pos_button.addEventListener("click", show_cat);
-}
-
-function button_right()
-{
-    let pos_old_button = document.getElementsByClassName('menu')[0];
-    pos_old_button.remove()
-    index_button = index_button + 1;
-    if( index_button >= all_categories.length)
-        index_button = 0;
-    all_categories[index_button].gen_button();
-    let pos_button = document.getElementsByClassName('menu')[0];
-    pos_button.addEventListener("click", show_cat);
-}
-
-function show_cat()
-{
-    let pos_old_article = document.getElementsByClassName('article');
-    let name_cat = all_categories[index_button].name;
-
-    while (pos_old_article.length != 0)
-    {
-        pos_old_article[0].remove();
-    }
-
-    for (let elem of all_prod)
-    {
-        tmp_all_categories = elem.category;
-        for (let single_cat of tmp_all_categories)
-        {
-            if (single_cat == name_cat)
-            {
-                elem.gen_card();
-            }
-        }
-    }
-}
-
 //Buttons for carrousele picture
 let pos_buttons_car = document.getElementsByClassName('fl_gr');
-let pos_button_left_car = pos_buttons_car[0];
-let pos_button_right_car = pos_buttons_car[1];
-pos_button_left_car.addEventListener("click", car_left);
-pos_button_right_car.addEventListener('click', car_right);
+pos_buttons_car[0].addEventListener("click",  function(){move_car_up(-1);});
+pos_buttons_car[1].addEventListener('click', function(){move_car_up(1);});
 
 //Buttons for carrousele categories' button
 let pos_buttons_cat = document.getElementsByClassName('fl_pt');
-let pos_button_left_cat = pos_buttons_cat[0];
-let pos_button_right_cat = pos_buttons_cat[1];
-pos_button_left_cat.addEventListener('click', button_left);
-pos_button_right_cat.addEventListener('click', button_right);
+pos_buttons_cat[0].addEventListener('click', function(){move_car_down(-1);});
+pos_buttons_cat[1].addEventListener('click', function(){move_car_down(1);});
 
 //Creation par defaut des buttons et images
 all_categories[0].gen_button();
 all_prod[0].gen_picture();    
 
+//Ajoute la fonction de l'affichage de la commande actuelle 
+let pos_prim_order = document.getElementsByTagName('ul')[0];
+let pos_order = pos_prim_order.getElementsByTagName('li')[pos_prim_order.getElementsByTagName('li').length - 1];
+pos_order.addEventListener("click", show_order);
+
+//Ajoute la fonction de l'affichage lors du clic
 let pos_button = document.getElementsByClassName('menu')[0];
 pos_button.addEventListener("click", show_cat);
 
 show_cat();
-
-function show_order()
-{
-    console.log(test);
-    let somme = 0;
-    for (let elem of test)
-        somme = somme + parseInt(elem.price);
-    console.log(somme);
-    
-}
-
-let pos_prim_order = document.getElementsByTagName('ul')[0];
-let pos_order = pos_prim_order.getElementsByTagName('li')[pos_prim_order.getElementsByTagName('li').length - 1];
-
-pos_order.addEventListener("click", show_order);
-
